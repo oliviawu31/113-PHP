5b2e3f94dc7f6e0dcdcf0e683f4526e71659460b
oliviawu <oliviawu0301@gmail.com>
2024-10-28 11:12:09 +0800

    add BMI結果頁面

--- index.php
+++ index.php
@@ -4,8 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>計算BMI</title>
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: lightblue;
+            margin: 0;
+            padding: 20px;
+        }
+       
+</style>
+
 </head>
 <body>
     <h1>計算BMI</h1>
+    <form action="result.php" method="post">
+        <div>
+        <label for="height">身高 (公分):</label>
+        <input type="number" name="height" id="height">
+        </div>
+        <div>
+        <label for="weight">體重 (公斤):</label>
+        <input type="number" name="weight" id="height">
+        </div>
+        <!-- input:submit+input:rest
+        <input type="submit" value="">
+        <input type="reset" value=""> -->
+        <div>
+        <input type="submit" value="計算">
+        <input type="reset" value="清空/重置">
+        </div>
+
 </body>
 </html>
---
+++ result.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BMI結果</title>
+</head>
+<body>
+    <h1>BMI結果</h1>
+    <div>您的身高:公分</div>
+    <div>您的體重:公斤</div>
+    <div>您的BMI為:</div>    
+    <div>體位判定為:</div>
+    <div>
+        <a href="index.php">回首頁/重新測量</a>
+    </div>
+
+</body>
+</html>
